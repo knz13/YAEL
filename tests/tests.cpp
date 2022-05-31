d0c5d4210ca19f60193491d5c3ba4111ceb38301
knz13 <otaviomaya@gmail.com>
2022-05-30 23:59:41 -0300

    Adding the library files

---
+++ tests/tests.cpp
@@ -0,0 +1,175 @@
+#include "../yael.h"
+#include "catch2/catch_test_macros.hpp"
+
+
+struct TestLauncher {
+
+public:
+    yael::event_sink<void()> onVoidEvent() {
+        return {m_VoidEvent};
+    }
+    yael::event_sink<void(int)> onVoidIntEvent(){
+        return {m_VoidIntEvent};
+    };
+
+    yael::event_sink<int(std::string)> onNonVoidEvent() {
+        return {m_NonVoidEvent};
+    }
+
+
+    yael::event_launcher<void()> m_VoidEvent;
+    yael::event_launcher<void(int)> m_VoidIntEvent;
+    yael::event_launcher<int(std::string)> m_NonVoidEvent;
+
+
+};
+
+struct TestReceiver : public yael::event_receiver {
+
+};
+
+struct TestReceiverDerived : public TestReceiver {
+
+};
+ 
+
+TEST_CASE("Event Testing With Receiver"){
+    TestLauncher launcher;
+    TestReceiver receiver;
+
+    SECTION("Subscribing test"){
+        launcher.onVoidEvent().Connect(&receiver,[&](yael::event_receiver* eventReceiver){
+            REQUIRE(&receiver == eventReceiver);
+        });
+
+        launcher.onVoidIntEvent().Connect(&receiver,[&](yael::event_receiver* eventReceiver,int value){
+            REQUIRE(&receiver == eventReceiver);
+            REQUIRE(value == 1);
+        });
+        
+        launcher.onNonVoidEvent().Connect(&receiver,[&](yael::event_receiver* eventReceiver,std::string strValue){
+            REQUIRE(strValue == "my string");
+            REQUIRE(&receiver == eventReceiver);
+
+            return 1;
+        });
+
+        SECTION("Launching events"){
+            launcher.m_VoidEvent.EmitEvent();
+            launcher.m_VoidIntEvent.EmitEvent(1);
+            REQUIRE(launcher.m_NonVoidEvent.EmitEvent("my string")[0] == 1);
+        }
+        SECTION("Unsubscribing With One Receiver") {
+            REQUIRE(launcher.onVoidEvent().Disconnect(&receiver));
+            REQUIRE(launcher.onVoidIntEvent().Disconnect(&receiver));
+            REQUIRE(launcher.onNonVoidEvent().Disconnect(&receiver));
+        }
+
+        SECTION("Multiple receivers") {
+            TestReceiver receiverTwo;
+
+            REQUIRE(launcher.m_NonVoidEvent.NumberOfReceivers() == 1);
+
+            launcher.onNonVoidEvent().Connect(&receiverTwo,[](auto receiverPointer,std::string val){
+                return 2;
+            });
+
+            REQUIRE(launcher.m_NonVoidEvent.NumberOfReceivers() == 2);
+
+            std::vector<int> values = launcher.m_NonVoidEvent.EmitEvent("my string");
+            
+            REQUIRE(values.size() == 2);
+
+            REQUIRE(launcher.onNonVoidEvent().Disconnect(&receiverTwo));
+
+            REQUIRE(launcher.m_NonVoidEvent.NumberOfReceivers() == 1);
+            
+        }
+
+        SECTION("Mutiple receivers with death of a receiver") {
+            {
+                TestReceiver receiverTwo;
+                REQUIRE(launcher.m_VoidEvent.NumberOfReceivers() == 1);
+                launcher.onVoidEvent().Connect(&receiverTwo, [&receiverTwo](auto receiverPointer) {
+                    REQUIRE(&receiverTwo == receiverPointer);
+                });
+                REQUIRE(launcher.m_VoidEvent.NumberOfReceivers() == 2);
+                launcher.m_VoidEvent.EmitEvent();
+            }
+            REQUIRE(launcher.m_VoidEvent.NumberOfReceivers() == 1);
+
+            launcher.m_VoidEvent.EmitEvent();
+        }
+
+    }
+
+}
+
+
+TEST_CASE("Launcher Death") {
+    TestReceiver receiver;
+
+    {
+        TestLauncher launcher;
+
+        launcher.onVoidEvent().Connect(&receiver, [](auto pointer) {});
+
+        REQUIRE(receiver.IsSubscribedTo(launcher.onVoidEvent()));
+        REQUIRE(receiver.NumberOfSubscribedEvents() == 1);
+    }
+
+    REQUIRE(receiver.NumberOfSubscribedEvents() == 0);
+   
+
+}
+
+TEST_CASE("Launcher Death With Derived Receiver") {
+    TestReceiverDerived receiver;
+
+    {
+        TestLauncher launcher;
+
+        launcher.onVoidEvent().Connect(&receiver, [](auto rec) {});
+
+        REQUIRE(receiver.IsSubscribedTo(launcher.onVoidEvent()));
+        REQUIRE(receiver.NumberOfSubscribedEvents() == 1);
+    }
+
+    REQUIRE(receiver.NumberOfSubscribedEvents() == 0);
+}
+
+TEST_CASE("Copying Receiver") {
+    TestReceiver receiverOne;
+    TestReceiver receiverTwo;
+    TestLauncher launcher;
+
+    launcher.onVoidEvent().Connect(&receiverOne, [](auto ptr) {});
+
+    REQUIRE(launcher.m_VoidEvent.NumberOfReceivers() == 1);
+    REQUIRE(launcher.m_VoidEvent.IsReceiverConnected(&receiverOne));
+    REQUIRE(!launcher.m_VoidEvent.IsReceiverConnected(&receiverTwo));
+    REQUIRE(receiverOne.IsSubscribedTo(launcher.onVoidEvent()));
+    REQUIRE(!receiverTwo.IsSubscribedTo(launcher.onVoidEvent()));
+
+    receiverTwo = receiverOne;
+
+    REQUIRE(!receiverTwo.IsSubscribedTo(launcher.onVoidEvent()));
+    REQUIRE(launcher.m_VoidEvent.NumberOfReceivers() == 1);
+
+
+}
+
+TEST_CASE("Free function attachment") {
+    TestLauncher launcher;
+
+    size_t id = launcher.onVoidEvent().Connect([](){});
+
+    REQUIRE(launcher.onVoidEvent().NumberOfReceivers() == 1);
+    REQUIRE(launcher.onVoidEvent().IsReceiverConnected(id));
+
+    REQUIRE(launcher.onVoidEvent().Disconnect(id));
+    REQUIRE(launcher.onVoidEvent().NumberOfReceivers() == 0);
+
+
+
+}
---
+++ yael.h
@@ -0,0 +1,212 @@
+#pragma once
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <typeinfo>
+
+
+namespace yael {
+struct event_receiver;
+    struct event_receiver_attachment_properties {
+        std::function<void(void*, size_t)> m_DeleteFunc;
+    };
+
+
+    template<typename T>
+    struct event_launcher;
+    template<typename T>
+    struct event_sink;
+
+    struct event_receiver {
+
+        ~event_receiver() {
+            if (m_SubscribedEvents.size() == 0) {
+                return;
+            }
+            auto it = m_SubscribedEvents.begin();
+            while (it != m_SubscribedEvents.end()) {
+                it->second.m_DeleteFunc(it->first, std::hash<void*>()((void*)this));
+                it = m_SubscribedEvents.begin();
+            }
+        }
+
+        template<typename T>
+        bool IsSubscribedTo(event_sink<T> sink) {
+            return sink.m_Master->IsReceiverConnected(this);
+        }
+        
+        size_t NumberOfSubscribedEvents() const {
+            return m_SubscribedEvents.size();
+        }
+
+
+        event_receiver& operator=(const event_receiver& other) {
+            return *this;
+        }
+
+    private:
+        std::map<void*, event_receiver_attachment_properties> m_SubscribedEvents;
+
+        template<typename T>
+        friend struct event_sink;
+        template<typename T>
+        friend struct event_launcher;
+    };
+
+
+    template<typename R, typename... Args>
+    struct event_launcher<R(Args...)> {
+
+        event_launcher() {
+        };
+
+        ~event_launcher() {
+        }
+
+        size_t NumberOfReceivers() const {
+            return m_Receivers.size();
+        }
+
+        bool IsReceiverConnected(event_receiver* rec) const {
+            size_t hash = std::hash<void*>()((void*)rec);
+            return m_Receivers.find(hash) != m_Receivers.end();
+        }
+
+        bool IsReceiverConnected(size_t hash) const {
+            return m_Receivers.find(hash) != m_Receivers.end();
+        }
+
+        bool DisconnectReceiver(size_t hash) {
+            if (m_Receivers.find(hash) != m_Receivers.end()) {
+                m_Receivers.erase(hash);
+                return true;
+            }
+            return false;
+        };
+
+        void Clear() {
+            m_Receivers.clear();
+        }
+
+
+        auto EmitEvent(Args... args) {
+            if constexpr (std::is_same<R,void>::value) {
+                for (auto& [handle, func] : m_Receivers) {
+                    if (func) {
+                        (*func.get())(args...);
+                    }
+                }
+            }
+            else {
+                std::vector<R> vec;
+                for (auto& [handle, func] : m_Receivers) {
+                    if (func) {
+                        vec.push_back((*func.get())(args...));
+                    }
+                }
+                return vec;
+            }
+        };
+
+        event_launcher<R(Args...)>& operator=(const event_launcher<R(Args...)>& other) {
+            return *this;
+        }
+
+    private:
+        std::unordered_map<size_t, std::shared_ptr<std::function<R(Args...)>>> m_Receivers;
+        size_t m_MyHash = std::hash<void*>()((void*)this);
+
+        template<typename T>
+        friend struct event_sink;
+
+    };
+
+
+
+
+
+
+
+    template<typename R, typename... Args>
+    struct event_sink<R(Args...)> {
+        event_sink(event_launcher<R(Args...)>& sink) : m_Master(&sink) {};
+
+        size_t Connect(std::function<R(Args...)> windowFunc) {
+            static int count = 1;
+            size_t hash = std::hash<int>()(count);
+            count++;
+
+            std::function<R(Args...)>* func = new std::function<R(Args...)>(windowFunc);
+            m_Master->m_Receivers[hash] = std::make_shared<std::function<R(Args...)>>(*func);
+            return hash;
+
+        }
+
+        void Connect(event_receiver* key, std::function<R(event_receiver*, Args...)> windowFunc) {
+
+            size_t hash = std::hash<void*>()((void*)key);
+            auto deleter = [=](std::function<R(Args...)>* func) {
+                key->m_SubscribedEvents.erase((void*)m_Master);
+                delete func;
+            };
+
+            auto func = new std::function<R(Args...)>([=](auto... args) {
+                return windowFunc(key, args...);
+            });
+            m_Master->m_Receivers[hash] = std::shared_ptr<std::function<R(Args...)>>(func, deleter);
+
+            event_receiver_attachment_properties prop;
+           
+
+            prop.m_DeleteFunc = [](void* ptr, size_t hash) {
+                ((event_launcher<R(Args...)>*)(ptr))->DisconnectReceiver(hash);
+            };
+
+            key->m_SubscribedEvents[(void*)m_Master] = std::move(prop);
+
+        };
+
+        bool Disconnect(size_t hashKey) {
+            return m_Master->DisconnectReceiver(hashKey);
+        }
+
+        bool IsReceiverConnected(event_receiver* rec) const {
+            return m_Master->IsReceiverConnected(rec);
+        }
+
+        bool IsReceiverConnected(size_t hash) const {
+            return m_Master->IsReceiverConnected(hash);
+        }
+
+        size_t NumberOfReceivers() const {
+            return m_Master->NumberOfReceivers();
+        }
+
+        bool Disconnect(event_receiver* key) {
+            if (key == nullptr) {
+                return false;
+            }
+            size_t hash = std::hash<void*>()((void*)key);
+            if (m_Master->m_Receivers.find(hash) != m_Master->m_Receivers.end()) {
+                m_Master->DisconnectReceiver(hash);
+            }
+            else {
+                return false;
+            }
+            return true;
+        }
+
+    private:
+
+
+
+        event_launcher<R(Args...)>* m_Master;
+
+
+        friend class event_receiver;
+
+
+    };
+
+};
